090b4eadede23799c79d924680a89a4f18add150
kky0426 <skwndb15@gmail.com>
2023-12-04 11:42:22 +0900

    fix: logo padding

--- superset-frontend/src/features/home/Menu.tsx
+++ superset-frontend/src/features/home/Menu.tsx
@@ -59,9 +59,9 @@ const StyledHeader = styled.header`
         justify-content: center;
         /* must be exactly the height of the Antd navbar */
         min-height: 50px;
-        padding: ${theme.gridUnit}px
+        padding: ${theme.gridUnit * 2}px
           ${theme.gridUnit * 2}px
-          ${theme.gridUnit}px
+          ${theme.gridUnit * 2}px
           ${theme.gridUnit * 4}px;
         max-width: ${theme.gridUnit * theme.brandIconMaxWidth}px;
         img {
